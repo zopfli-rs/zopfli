133fb4e6eb4a8f24c977552ecbf1d75a1d054389
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-27 17:36:20 -0400

    Have a representation of ZopfliOptions defined in Rust

---
+++ src/zopfli/zopfli.rs
@@ -0,0 +1,31 @@
+use libc::c_int;
+
+/// Options used throughout the program.
+#[repr(C)]
+pub struct ZopfliOptions {
+  /* Whether to print output */
+  verbose: c_int,
+  /* Whether to print more detailed output */
+  verbose_more: c_int,
+  /*
+  Maximum amount of times to rerun forward and backward pass to optimize LZ77
+  compression cost. Good values: 10, 15 for small files, 5 for files over
+  several MB in size or it will be too slow.
+  */
+  numiterations: c_int,
+  /*
+  If true, splits the data in multiple deflate blocks with optimal choice
+  for the block boundaries. Block splitting gives better compression. Default:
+  true (1).
+  */
+  blocksplitting: c_int,
+  /*
+  No longer used, left for compatibility.
+  */
+  blocksplittinglast: c_int,
+  /*
+  Maximum amount of blocks to split into (0 for unlimited, but this can give
+  extreme results that hurt compression on some files). Default value: 15.
+  */
+  blocksplittingmax: c_int,
+}
