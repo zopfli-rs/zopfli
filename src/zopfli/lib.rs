66705b59cacaf5b063a330f2ce9021a1b23adf48
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-27 16:55:35 -0400

    Make ZopfliCalculateEntropy be a sub-function of SymbolStats

--- src/zopfli/lib.rs
+++ src/zopfli/lib.rs
@@ -3,4 +3,3 @@ extern crate libc;
 pub mod lz77;
 pub mod squeeze;
 pub mod symbols;
-pub mod tree;
--- src/zopfli/squeeze.rs
+++ src/zopfli/squeeze.rs
@@ -1,7 +1,10 @@
+use std::slice;
+
 use libc::{c_void, c_uint, c_double, c_int, size_t};
 
 use symbols::{ZopfliGetDistExtraBits, ZopfliGetLengthExtraBits, ZopfliGetLengthSymbol, ZopfliGetDistSymbol, ZOPFLI_NUM_LL, ZOPFLI_NUM_D};
-use tree::ZopfliCalculateEntropy;
+
+const K_INV_LOG2: c_double = 1.4426950408889;  // 1.0 / log(2.0)
 
 /// Cost model which should exactly match fixed tree.
 /// type: CostModelFun
@@ -117,9 +120,54 @@ impl SymbolStats {
         self.litlens[256] = 1; // End symbol.
     }
 
+    /// Calculates the entropy of each symbol, based on the counts of each symbol. The
+    /// result is similar to the result of ZopfliCalculateBitLengths, but with the
+    /// actual theoritical bit lengths according to the entropy. Since the resulting
+    /// values are fractional, they cannot be used to encode the tree specified by
+    /// DEFLATE.
     pub fn calculate_entropy(&mut self) {
-        ZopfliCalculateEntropy(self.litlens.as_ptr(), ZOPFLI_NUM_LL as size_t, self.ll_symbols.as_mut_ptr());
-        ZopfliCalculateEntropy(self.dists.as_ptr(), ZOPFLI_NUM_D as size_t, self.d_symbols.as_mut_ptr());
+        fn calculate_and_store_entropy(count_ptr: *const size_t, n: size_t, bitlengths_ptr: *mut c_double) {
+            let count = unsafe {
+                assert!(!count_ptr.is_null());
+                slice::from_raw_parts(count_ptr, n as usize)
+            };
+            let bitlengths = unsafe {
+                assert!(!bitlengths_ptr.is_null());
+                slice::from_raw_parts_mut(bitlengths_ptr, n as usize)
+            };
+
+            let mut sum = 0;
+            for i in 0..(n as usize) {
+                sum += count[i];
+            }
+
+            let log2sum = (if sum == 0 { n } else { sum } as c_double).ln() * K_INV_LOG2;
+
+            for i in 0..n as usize {
+                // When the count of the symbol is 0, but its cost is requested anyway, it
+                // means the symbol will appear at least once anyway, so give it the cost as if
+                // its count is 1.
+                if count[i] == 0 {
+                    bitlengths[i] = log2sum;
+                } else {
+                    bitlengths[i] = log2sum - (count[i] as c_double).ln() * K_INV_LOG2;
+                }
+
+                // Depending on compiler and architecture, the above subtraction of two
+                // floating point numbers may give a negative result very close to zero
+                // instead of zero (e.g. -5.973954e-17 with gcc 4.1.2 on Ubuntu 11.4). Clamp
+                // it to zero. These floating point imprecisions do not affect the cost model
+                // significantly so this is ok.
+                if bitlengths[i] < 0.0 && bitlengths[i] > -1E-5 {
+                    bitlengths[i] = 0.0;
+                }
+                assert!(bitlengths[i] >= 0.0);
+            }
+        }
+
+        calculate_and_store_entropy(self.litlens.as_ptr(), ZOPFLI_NUM_LL as size_t, self.ll_symbols.as_mut_ptr());
+        calculate_and_store_entropy(self.dists.as_ptr(), ZOPFLI_NUM_D as size_t, self.d_symbols.as_mut_ptr());
+
     }
 }
 
