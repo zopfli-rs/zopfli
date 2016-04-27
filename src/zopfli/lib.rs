bd35d151e9ac14669b8e126dc7f033e55a7ef857
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-04-27 16:06:40 -0400

    Move ZopfliCalculateEntropy to rust

--- src/zopfli/lib.rs
+++ src/zopfli/lib.rs
@@ -3,3 +3,4 @@ extern crate libc;
 pub mod lz77;
 pub mod squeeze;
 pub mod symbols;
+pub mod tree;
--- src/zopfli/tree.c
+++ src/zopfli/tree.c
@@ -68,30 +68,7 @@ void ZopfliLengthsToSymbols(const unsigned* lengths, size_t n, unsigned maxbits,
   free(next_code);
 }
 
-void ZopfliCalculateEntropy(const size_t* count, size_t n, double* bitlengths) {
-  static const double kInvLog2 = 1.4426950408889;  /* 1.0 / log(2.0) */
-  unsigned sum = 0;
-  unsigned i;
-  double log2sum;
-  for (i = 0; i < n; ++i) {
-    sum += count[i];
-  }
-  log2sum = (sum == 0 ? log(n) : log(sum)) * kInvLog2;
-  for (i = 0; i < n; ++i) {
-    /* When the count of the symbol is 0, but its cost is requested anyway, it
-    means the symbol will appear at least once anyway, so give it the cost as if
-    its count is 1.*/
-    if (count[i] == 0) bitlengths[i] = log2sum;
-    else bitlengths[i] = log2sum - log(count[i]) * kInvLog2;
-    /* Depending on compiler and architecture, the above subtraction of two
-    floating point numbers may give a negative result very close to zero
-    instead of zero (e.g. -5.973954e-17 with gcc 4.1.2 on Ubuntu 11.4). Clamp
-    it to zero. These floating point imprecisions do not affect the cost model
-    significantly so this is ok. */
-    if (bitlengths[i] < 0 && bitlengths[i] > -1e-5) bitlengths[i] = 0;
-    assert(bitlengths[i] >= 0);
-  }
-}
+extern void ZopfliCalculateEntropy(const size_t* count, size_t n, double* bitlengths);
 
 void ZopfliCalculateBitLengths(const size_t* count, size_t n, int maxbits,
                                unsigned* bitlengths) {
