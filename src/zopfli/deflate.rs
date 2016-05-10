aca67fef25a630d23c33dabfeab8befb8dea47e6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-05-10 18:26:26 -0400

    Move OptimizeHuffmanForRle to rust

--- src/zopfli/deflate.rs
+++ src/zopfli/deflate.rs
@@ -1,4 +1,4 @@
-use libc::{c_uint};
+use libc::{c_uint, c_int, size_t};
 
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -18,6 +18,106 @@ pub extern fn GetFixedTree(ll_lengths: *mut c_uint, d_lengths: *mut c_uint) {
         }
         for i in 0..32 {
             *d_lengths.offset(i) = 5;
+        }
+    }
+}
+
+
+/// Change the population counts in a way that the consequent Huffman tree
+/// compression, especially its rle-part will be more likely to compress this data
+/// more efficiently. length containts the size of the histogram.
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern fn OptimizeHuffmanForRle(length: c_int, counts: *mut size_t) {
+    let mut length = length;
+
+    // 1) We don't want to touch the trailing zeros. We may break the
+    // rules of the format by adding more data in the distance codes.
+    while length >= 0 {
+        if length == 0 {
+            return;
+        }
+        if unsafe { *counts.offset((length - 1) as isize) } != 0 {
+            // Now counts[0..length - 1] does not have trailing zeros.
+            break;
+        }
+        length -= 1;
+    }
+
+    // 2) Let's mark all population counts that already can be encoded
+    // with an rle code.
+    let mut good_for_rle = vec![0; length as size_t];
+
+    // Let's not spoil any of the existing good rle codes.
+    // Mark any seq of 0's that is longer than 5 as a good_for_rle.
+    // Mark any seq of non-0's that is longer than 7 as a good_for_rle.
+    let mut symbol = unsafe { *counts.offset(0) };
+    let mut stride = 0;
+    for i in 0..(length + 1) {
+        let ci = unsafe { *counts.offset(i as isize) };
+        if i == length || ci != symbol {
+            if (symbol == 0 && stride >= 5) || (symbol != 0 && stride >= 7) {
+                for k in 0..stride {
+                    good_for_rle[(i - k - 1) as usize] = 1;
+                }
+            }
+            stride = 1;
+            if i != length {
+                symbol = ci;
+            }
+        } else {
+            stride += 1;
+        }
+    }
+
+    // 3) Let's replace those population counts that lead to more rle codes.
+    stride = 0;
+    let mut limit = unsafe { *counts.offset(0) };
+    let mut sum = 0;
+    for i in 0..(length + 1) {
+        let ci = unsafe { *counts.offset(i as isize) };
+
+        // Heuristic for selecting the stride ranges to collapse.
+        if i == length || good_for_rle[i as usize] != 0 || (ci as i32 - limit as i32).abs() >= 4 {
+            if stride >= 4 || (stride >= 3 && sum == 0) {
+                // The stride must end, collapse what we have, if we have enough (4).
+                let mut count = (sum + stride / 2) / stride;
+                if count < 1 {
+                    count = 1;
+                }
+                if sum == 0 {
+                    // Don't make an all zeros stride to be upgraded to ones.
+                    count = 0;
+                }
+                for k in 0..stride {
+                    // We don't want to change value at counts[i],
+                    // that is already belonging to the next stride. Thus - 1.
+                    unsafe { *counts.offset((i - k - 1) as isize) = count as size_t };
+                }
+            }
+            stride = 0;
+            sum = 0;
+            if i < length - 3 {
+                // All interesting strides have a count of at least 4,
+                // at least when non-zeros.
+                unsafe {
+                    limit = (
+                        ci +
+                        *counts.offset((i + 1) as isize) +
+                        *counts.offset((i + 2) as isize) +
+                        *counts.offset((i + 3) as isize) +
+                        2
+                    ) / 4;
+                }
+            } else if i < length {
+                limit = ci;
+            } else {
+                limit = 0;
+            }
+        }
+        stride += 1;
+        if i != length {
+            sum += ci as i32;
         }
     }
 }
