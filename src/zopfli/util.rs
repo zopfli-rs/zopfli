f1525a71d526bd3f37226e37777f7872fe9eb5b7
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-06-01 11:01:42 -0400

    Remove unused constants; move documentation to rust

--- src/zopfli/util.rs
+++ src/zopfli/util.rs
@@ -1,14 +1,40 @@
 use libc::{size_t, c_double};
 
+/// Number of distinct literal/length symbols in DEFLATE
 pub const ZOPFLI_NUM_LL: size_t = 288;
+/// Number of distinct distance symbols in DEFLATE
 pub const ZOPFLI_NUM_D: size_t = 32;
 
+/// The window size for deflate. Must be a power of two. This should be 32768, the
+/// maximum possible by the deflate spec. Anything less hurts compression more than
+/// speed.
 pub const ZOPFLI_WINDOW_SIZE: size_t = 32768;
+
+/// The window mask used to wrap indices into the window. This is why the
+/// window size must be a power of two.
 pub const ZOPFLI_WINDOW_MASK: size_t = 32767; // ZOPFLI_WINDOW_SIZE - 1
+
+/// Maximum length that can be encoded in deflate.
 pub const ZOPFLI_MAX_MATCH: size_t = 258;
+/// Minimum length that can be encoded in deflate.
 pub const ZOPFLI_MIN_MATCH: size_t = 3;
+
+/// For longest match cache. max 256. Uses huge amounts of memory but makes it
+/// faster. Uses this many times three bytes per single byte of the input data.
+/// This is so because longest match finding has to find the exact distance
+/// that belongs to each length for the best lz77 strategy.
+/// Good values: e.g. 5, 8.
 pub const ZOPFLI_CACHE_LENGTH: size_t = 8;
+
+/// limit the max hash chain hits for this hash value. This has an effect only
+/// on files where the hash value is the same very often. On these files, this
+/// gives worse compression (the value should ideally be 32768, which is the
+/// ZOPFLI_WINDOW_SIZE, while zlib uses 4096 even for best level), but makes it
+/// faster on some specific files.
+/// Good value: e.g. 8192.
 pub const ZOPFLI_MAX_CHAIN_HITS: size_t = 8192;
+
+/// Used to initialize costs for example
 pub const ZOPFLI_LARGE_FLOAT: c_double = 1E30;
 
 /// A block structure of huge, non-smart, blocks to divide the input into, to allow
