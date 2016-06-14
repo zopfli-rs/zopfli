3781f4e2161300e04ef943be5fb44628f741cf69
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-06-14 15:27:47 -0400

    Scope git-diff test failure to the test results
    So that if you have code changes that haven't been checked in yet, they
    don't trip the test failure

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eu
-./zopfli test/data/* && mv test/data/*.gz test/results/ && git diff --exit-code
+./zopfli test/data/* && mv test/data/*.gz test/results/ && git diff --exit-code test/results/
