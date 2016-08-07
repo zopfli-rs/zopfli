1a032dd77aa062b1734c7b3740e21b70afbc2753
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-08-07 10:53:52 -0400

    Pull arguments out into a constant - they won't change

--- src/deflate.rs
+++ src/deflate.rs
@@ -385,10 +385,21 @@ fn encode_tree_no_output(ll_lengths: &[u32], d_lengths: &[u32], use_16: bool, us
     result_size
 }
 
+static TRUTH_TABLE: [(bool, bool, bool); 8] = [
+    (false, false, false),
+    (true, false, false),
+    (false, true, false),
+    (true, true, false),
+    (false, false, true),
+    (true, false, true),
+    (false, true, true),
+    (true, true, true),
+];
+
 /// Gives the exact size of the tree, in bits, as it will be encoded in DEFLATE.
 fn calculate_tree_size(ll_lengths: &[u32], d_lengths: &[u32]) -> usize {
-    (0..8).map(|i| {
-        encode_tree_no_output(ll_lengths, d_lengths, i & 1 > 0, i & 2 > 0, i & 4 > 0)
+    TRUTH_TABLE.iter().map(|&(use_16, use_17, use_18)| {
+        encode_tree_no_output(ll_lengths, d_lengths, use_16, use_17, use_18)
     }).min().unwrap_or(0)
 }
 
