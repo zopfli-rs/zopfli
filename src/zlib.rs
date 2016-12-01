5577adc91b1c455ed5f8904742aefe482a6b33ba
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-11-30 20:44:26 -0500

    Fix Big/Little endianness :(
    Connects to #36.

--- src/zlib.rs
+++ src/zlib.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Write};
 use adler32::adler32;
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{BigEndian, WriteBytesExt};
 
 use deflate::{deflate, BlockType};
 use Options;
@@ -15,10 +15,10 @@ pub fn zlib_compress<W>(options: &Options, in_data: &[u8], mut out: W) -> io::Re
     let fcheck = 31 - cmfflg % 31;
     cmfflg += fcheck;
 
-    try!(out.by_ref().write_u16::<LittleEndian>(cmfflg));
+    try!(out.by_ref().write_u16::<BigEndian>(cmfflg));
 
     try!(deflate(options, BlockType::Dynamic, in_data, out.by_ref()));
 
     let checksum = adler32(io::Cursor::new(&in_data)).expect("Error with adler32");
-    out.write_u32::<LittleEndian>(checksum)
+    out.write_u32::<BigEndian>(checksum)
 }
