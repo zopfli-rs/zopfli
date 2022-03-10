edca85e68e98f080ec8e3bf536df218e7b99ed7e
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-03-09 22:08:24 -0600

    Overhaul test script
    * Test each file in its own command so we can report which failed
    * Validate that the resulting .gz file can be decompressed with `gz -d`
    * Verify that each compressed file decompresses to the original input
    * Don't overwrite the git-stored compressed files

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 set -eu
+set -o pipefail
 
-rm test/data/*.gz
-env RUST_BACKTRACE=1 ./zopfli test/data/*
-mv test/data/*.gz test/results/
-git diff --exit-code test/results/
+# Clean output from previous runs
+rm -f test/data/*.gz
+
+# Test all input cases individually so we can report which failed
+for input in test/data/*; do
+	printf "Compressing ${input}... "
+	env RUST_BACKTRACE=1 ./zopfli ${input} && echo "done"
+done
+
+# Move newly compressed data to its own directory
+mkdir -p test/temp_compressed/
+mv test/data/*.gz test/temp_compressed/
+
+# Compare newly compressed data with expected
+diff -r test/results/ test/temp_compressed/
+
+# Verify that all compressed files decompress back to the input sources
+mkdir -p test/temp_decompressed/
+for gz in test/results/*.gz; do
+	BASENAME=$(basename -s .gz $gz)
+	printf "Validating ${gz}... "
+	cat $gz | gzip -d > test/temp_decompressed/${BASENAME}
+	diff -r test/data/${BASENAME} test/temp_decompressed/${BASENAME} && echo "done"
+done
+
+# Clean up temporary output
+rm -rf test/temp_decompressed/
+rm -rf test/temp_compressed/
